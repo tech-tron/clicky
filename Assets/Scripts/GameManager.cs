0cbb3a41c38993901517c02c379e755611e03554
tech-tron <phillables@gmail.com>
2022-04-11 09:08:51 -0400

    restart button and function
    restart button added to canvas that on click calls the game manager which now has a restart function that just loads scene of the get active name

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -42,5 +43,9 @@ public class GameManager : MonoBehaviour
     public void GameOver(){
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
+    }
+
+    public void RestartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
