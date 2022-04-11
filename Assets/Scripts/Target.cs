aa6f9b8e1353010f611a66bd72f5f231ef33acfc
tech-tron <phillables@gmail.com>
2022-04-11 05:45:22 -0400

    destroy objects when clicked or on trigger enter

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -9,7 +9,7 @@ public class Target : MonoBehaviour
     private float maxSpeed = 16f;
     private float maxTorque = 10f;
     private float xRange = 4f;
-    private float ySpawnPos = -6f;
+    private float ySpawnPos = -2f;
 
 
     void Start()
@@ -24,6 +24,13 @@ public class Target : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnMouseUp(){
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other){
+        Destroy(gameObject);
     }
 
     Vector3 RandomForce(){
